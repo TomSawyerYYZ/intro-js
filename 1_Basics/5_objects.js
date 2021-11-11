533910b7fdbdb41930bc33f7b5dbd2eb1216584f
YOUR-NAME <lorenzo.sioli98@gmail.com>
2021-11-11 12:59:22 +0100

    commit objects

--- 1_Basics/5_objects.js
+++ 1_Basics/5_objects.js
@@ -42,12 +42,23 @@ console.log(typeof null);
 // decided that the type of null is 'object'.
 // Hint. The property name must contain the full name (Brendan Eich), and
 // the property birth must contain the year in which he was born (1961).
-
+person = {
+    name: 'Brendan Eich',
+    year: 1961
+};
+console.log(person);
 // b. Access the properties of the person object and create a sentence of the
 // type: "X was born in Y." where X is Brendan Eich and Y is 1961.
-
+console.log('the name is: ' + person.name);
+console.log('the year is: ' + person.year);
+console.log(person.name + ' was born in ' + person.year);
 // c. Bonus exercise. Instead of saying that year of birth, you could say that
 // Brendan Eich is Z years old.  
+person = {
+    name: 'brendan eich',
+    year: 1961
+};
+console.log(person.name + ' is ' + (2021 - person.year) + 'yo');
 
 
 // EXERCISE 2. Add and remove properties to the person object.
@@ -55,7 +66,11 @@ console.log(typeof null);
 
 // Now you realize that it makes more sense to split the property 'name' into
 // two: 'first' and 'last' name. Accordingly you delete the propery name.
+person.first = 'Brendan';
+person.last = 'Eich';
+delete person.name;
 
+console.log(person);
 
 
 // EXERCISE 3. Bonus. Constant objects.
