0dec1ab45f1b94ccb9ae6b79eceb01b5fa03bda1
YOUR-NAME <lorenzo.sioli98@gmail.com>
2021-11-11 15:57:22 +0100

    commit loops

--- 1_Basics/7_loops.js
+++ 1_Basics/7_loops.js
@@ -20,7 +20,9 @@ persons = [
     { first: 'Napoleon', last: 'Bonaparte', year: 1821 },
     { first: 'Linus', last: 'Torvalds', year: 1962 }
 ];
-
+for (let i=0; i < persons.length; i++) {
+console.log(persons[i].year);
+}
 
 
 // EXERCISE 2. Verbose Loop.
@@ -35,6 +37,16 @@ persons = [
 // you loop through the items in the array.
 // Hint2: You will also need some if logic to correctly add or not the comma
 // between the first and the second element and finishing with a dot.
+persons = [
+    { first: 'Brendan', last: 'Eich', year: 1961 },
+    { first: 'Pablo', last: 'Picasso', year: 1881 },
+    { first: 'Napoleon', last: 'Bonaparte', year: 1821 },
+    { first: 'Linus', last: 'Torvalds', year: 1962 }
+];
+
+paragraph = 'There are ${persons.length} elements in the array: ';
+
+
 
 // b. bonus. Can you replace the part "element 1" with "the first element" and
 // "element 2" with the "second element" and so on?
